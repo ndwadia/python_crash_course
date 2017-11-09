fc36857a2c2ed279c0ecea5a33ccfd74e447325b
Neville <ndwadia@gmail.com>
2017-11-09 19:26:20 +0530

    Modified file to make test functions clearer

--- ch11/test_survey.py
+++ ch11/test_survey.py
@@ -16,22 +16,22 @@ class TestAnonymousSurvey(unittest.TestCase):
         """
         question = "What language did you first learn to speak?"
         self.my_survey = AnonymousSurvey(question)
-        self.responses = ['English', 'Spanish', 'Mandarin']
+        self.test_responses = ['English', 'Spanish', 'Mandarin']
 
 
     def test_store_single_response(self):
         """Test that a single response is stored properly."""
-        self.my_survey.store_response(self.responses[1])
-        self.assertIn(self.responses[1], self.my_survey.responses)
+        self.my_survey.store_response(self.test_responses[1])
+        self.assertIn(self.test_responses[1], self.my_survey.responses)
         #print(self.my_survey.show_question())
         #print(self.my_survey.show_results())
 
     def test_store_three_responses(self):
         """Test that three individual responses are stored properly."""
-        for response in self.responses:
-            self.my_survey.store_response(response)
-        for response in self.responses:
-            self.assertIn(response, self.my_survey.responses)
+        for test_response in self.test_responses:
+            self.my_survey.store_response(test_response)
+        for test_response in self.test_responses:
+            self.assertIn(test_response, self.my_survey.responses)
         #print(self.my_survey.show_question())
         #print(self.my_survey.show_results())
 
