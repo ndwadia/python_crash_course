d4bd8e97e38f6ab15170fdabe74e040bc7e54923
Neville <ndwadia@gmail.com>
2017-11-11 11:41:11 +0530

    Initial commit

---
+++ ch15/die.py
@@ -0,0 +1,18 @@
+# pylint: disable=invalid-name
+# pylint: disable=too-few-public-methods
+"""
+Working with pygal
+"""
+
+from random import randint
+
+class Die():
+    """A class representing a single die."""
+
+    def __init__(self, num_sides=6):
+        """Assume a six-sided die."""
+        self.num_sides = num_sides
+
+    def roll(self):
+        """"Return a random value between 1 and number of sides."""
+        return randint(1, self.num_sides)
